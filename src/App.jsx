f0640218467e9a5e082ce2cff35fef0e5d925392
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-12-07 14:07:01 -0500

    added movie list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
-import Header from './components/Header/Header'
+import Header from "./components/Header/Header";
+import MovieList from "./components/MoviesList/MovieList";
 
 function App() {
-  return <div className="App">
-    <Header/>
-  </div>;
+  return (
+    <div className="App">
+      <Header />
+      <MovieList />
+    </div>
+  );
 }
 
 export default App;
